630aaddda8e79ac706e5e3fbf6f1d69054d4a219
Troy <me@troyb.xyz>
2023-08-25 22:01:11 +0000

    add spot orders

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ app = SilverBackApp()
 
 # Get the perps proxy contract
 PerpsMarket = project.PerpsMarketProxy.at('0xf272382cB3BE898A8CdB1A23BE056fA2Fcf4513b')
+SpotMarket = project.SpotMarketProxy.at('0x5FF4b3aacdeC86782d8c757FAa638d8790799E83')
 
 # Can handle some stuff on startup, like loading a heavy model or something
 @app.on_startup()
@@ -43,9 +44,9 @@ def exec_block(block: BlockAPI):
     print("NEW BLOCK: ", block.number)
     return {"message": f"Received block number {block.number}"}
 
-# Order keeper
-# settle function
-def settle_order(event):
+# Perps orders
+# settle perps order function
+def settle_perps_order(event):
     account_id = event['accountId']
     market_id = event['marketId']
     market_name = snx.perps.markets_by_id[market_id]["market_name"]
@@ -54,10 +55,27 @@ def settle_order(event):
     snx.perps.settle_pyth_order(account_id, submit=True)
 
 @app.on_(PerpsMarket.OrderCommitted, new_block_timeout=5)
+def perps_order_committed(event):
+    print(f"Perps order committed: {event}")
+    settle_perps_order(event)
+    return {"message": f"Perps order committed: {event}"}
+
+# Spot orders
+# settle spot order function
+def settle_spot_order(event):
+    market_id = event['marketId']
+    market_id, market_name = snx.spot._resolve_market(market_id=market_id, market_name=None)
+
+    async_order_id = event['asyncOrderId']
+    
+    snx.logger.info(f'Settling order {async_order_id} for market {market_name}')
+    snx.spot.settle_pyth_order(async_order_id, market_id=market_id, submit=True)
+
+@app.on_(SpotMarket.OrderCommitted, new_block_timeout=5)
 def order_committed(event):
-    print(f"Order committed: {event}")
-    settle_order(event)
-    return {"message": f"Order committed: {event}"}
+    print(f"Spot order committed: {event}")
+    settle_spot_order(event)
+    return {"message": f"Spot order committed: {event}"}
 
 
 # Just in case you need to release some resources or something
